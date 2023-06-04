a63e4027b9c3585848b5d581bf1f1307218d6559
alasdairforsythe <77910352+alasdairforsythe@users.noreply.github.com>
2023-06-04 18:24:07 +0700

    Fixed bug in incompleteutf8bytes

--- go/capcode.go
+++ go/capcode.go
@@ -65,15 +65,15 @@ func isModifier(r rune) bool {
 func IncompleteUTF8Bytes(bytes []byte) int {
     bytesLen := len(bytes)
     // Single byte or empty string
-	if bytesLen == 0 {
-		return 0
-	}
-    if  bytes[bytesLen - 1] & 0b10000000 == 0 {
+    if bytesLen == 0 {
+        return 0
+    }
+    if bytes[bytesLen-1]&0b10000000 == 0 {
         return 0
     }
     // Find the start of the last character sequence
     seqStart := bytesLen - 1
-    for seqStart >= 0 && (bytes[seqStart] & 0b11000000) == 0b10000000 {
+    for seqStart >= 0 && (bytes[seqStart]&0b11000000) == 0b10000000 {
         seqStart--
     }
     // If no sequence start found, all bytes are continuation bytes and thus are all incomplete
@@ -81,12 +81,22 @@ func IncompleteUTF8Bytes(bytes []byte) int {
         return bytesLen
     }
     // Determine expected sequence length from leading byte
-    seqLen := 0
-    for (bytes[seqStart] & (0b10000000 >> seqLen)) != 0 {
-        seqLen++
+    firstByte := bytes[seqStart]
+    var seqLen int
+    if (firstByte & 0b10000000) == 0 {
+        seqLen = 1
+    } else if (firstByte & 0b11100000) == 0b11000000 {
+        seqLen = 2
+    } else if (firstByte & 0b11110000) == 0b11100000 {
+        seqLen = 3
+    } else if (firstByte & 0b11111000) == 0b11110000 {
+        seqLen = 4
+    } else {
+        // This is not a valid UTF-8 starting byte
+        return bytesLen - seqStart
     }
     // If sequence length is larger than the remaining bytes, it's incomplete
-    if bytesLen - seqStart < seqLen {
+    if bytesLen-seqStart < seqLen {
         return seqLen - (bytesLen - seqStart)
     }
     return 0
